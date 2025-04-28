ad186d74242631eec09e151d8f2d20e22dd1f7db
Jo Dodd <jkdodd@users.noreply.github.com>
2025-04-28 16:51:30 +0100

    Polaroid output as .md

--- .eleventy.js
+++ .eleventy.js
@@ -4,43 +4,6 @@ module.exports = (config) => {
     config.addPassthroughCopy("src/images/");
 
     config.addWatchTarget("src/styles.css");
-
-    // config.addCollection("polaroids", (collection) =>
-    //   collection.getFilteredByGlob("polaroids/*.md")
-    // );
-  
-    // config.addFilter("sluggyJo", function (value) {
-    //   // lowercase and replace spaces with hyphens
-    //   return value
-    //     .toLowerCase()
-    //     .replace(/[\s~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g, "-");
-    // });
-  
-    // config.addFilter("pluck", function (arr, value, attr) {
-    //   return arr.filter((item) => item.data[attr] === value);
-    // });
-  
-    // config.addFilter("groupByYear", (collection) => {
-    //   return _.chain(collection)
-    //     .groupBy((post) => DateTime.fromJSDate(post.date).year)
-    //     .toPairs()
-    //     .reverse()
-    //     .value();
-    // });
-  
-    // config.addFilter("randomItem", (collection) => {
-    //   collection.sort(() => {
-    //     return 0.5 - Math.random();
-    //   });
-    //   return collection.slice(0, 12);
-    // });
-  
-    // const { execSync } = require("child_process");
-    // config.on("eleventy.after", () => {
-    //   execSync(`npx pagefind --site dist --glob \"**/*.html\"`, {
-    //     encoding: "utf-8",
-    //   });
-    // });
   
     return {
       markdownTemplateEngine: "njk",
