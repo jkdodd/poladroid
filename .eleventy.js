4ca5f0f47bedfbbf183a5b715b441155aace59a7
Jo Dodd <jkdodd@users.noreply.github.com>
2025-05-22 12:32:29 +0100

    Sorting

--- .eleventy.js
+++ .eleventy.js
@@ -5,24 +5,6 @@ module.exports = (config) => {
   // config.addWatchTarget("src/polaroid/*.md");
   config.addWatchTarget("src/styles.css");
 
-  // Sort by key
-  function sortByKey(values, key = "pagination_pageNumber") {
-    let vals = [...values];
-    return vals.sort((a, b) => {
-      const aVal = a?.data?.[key] ?? "";
-      const bVal = b?.data?.[key] ?? "";
-
-      // Falls es sich um Strings handelt, verwende localeCompare
-      if (typeof aVal === "string" && typeof bVal === "string") {
-        return aVal.localeCompare(bVal);
-      }
-
-      // Ansonsten gehe davon aus, dass es sich um Zahlen handelt
-      return Math.sign(aVal - bVal);
-    });
-  }
-  config.addFilter("sortByKey", sortByKey);
-
   return {
     markdownTemplateEngine: "njk",
     dataTemplateEngine: "njk",
