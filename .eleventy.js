c9f2f4bbe24e2565bfa12b4133e940a18f892b2d
Jo Dodd <jkdodd@users.noreply.github.com>
2025-05-22 11:45:17 +0100

    Sorting

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,37 @@
-
 module.exports = (config) => {
-  
-    config.addPassthroughCopy("src/styles.css");
-    config.addPassthroughCopy("src/images/*.png");
+  config.addPassthroughCopy("src/styles.css");
+  config.addPassthroughCopy("src/images/*.png");
 
-    // config.addWatchTarget("src/polaroid/*.md");
-    config.addWatchTarget("src/styles.css");
-  
-    return {
-      markdownTemplateEngine: "njk",
-      dataTemplateEngine: "njk",
-      htmlTemplateEngine: "njk",
-      dir: {
-        input: "src",
-        includes: "_includes",
-        output: "_site",
-        data: "_data",
-      },
-    };
+  // config.addWatchTarget("src/polaroid/*.md");
+  config.addWatchTarget("src/styles.css");
+
+  // Sort by key
+  function sortByKey(values, key = "pagination_pageNumber") {
+    let vals = [...values];
+    return vals.sort((a, b) => {
+      const aVal = a?.data?.[key] ?? "";
+      const bVal = b?.data?.[key] ?? "";
+
+      // Falls es sich um Strings handelt, verwende localeCompare
+      if (typeof aVal === "string" && typeof bVal === "string") {
+        return aVal.localeCompare(bVal);
+      }
+
+      // Ansonsten gehe davon aus, dass es sich um Zahlen handelt
+      return Math.sign(aVal - bVal);
+    });
+  }
+  config.addFilter("sortByKey", sortByKey);
+
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      includes: "_includes",
+      output: "_site",
+      data: "_data",
+    },
   };
-  
+};
